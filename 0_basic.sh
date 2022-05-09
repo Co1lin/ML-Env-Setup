45177c54c667abdfdc20688b62418f4c1b4aaee5
Colin <mail@co1in.me>
2022-05-09 17:23:58 +0800

    Update 0_basic.sh

--- 0_basic.sh
+++ 0_basic.sh
@@ -20,3 +20,14 @@ echo -e "set -g mouse on
 	> ~/.tmux.conf
 
 chsh -s /bin/zsh
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+cp ~/.oh-my-zsh/themes/robbyrussell.zsh-theme ~/.oh-my-zsh/themes/robbyrussell.zsh-theme.bak
+cp ./robbyrussell.zsh-theme ~/.oh-my-zsh/themes/robbyrussell.zsh-theme
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+cp ./zshrc ~/.zshrc
+source ~/.zshrc
